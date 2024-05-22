2565382893a6df2183f499a25f3f351d6134dc80
Cristofer Alan Angeles Soreano <cangeles@connectiveit.com.mx>
2024-05-21 21:23:07 -0600

    Finish login and register

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,30 +9,37 @@ import {
   Button
 } from "@nextui-org/react";
 
+import { useNavigate } from 'react-router-dom';
+
 const useVisible = () => {
   const [isVisible, setIsVisible] = useState(false);
-
   const toggleVisibility = () => setIsVisible(!isVisible);
-
   return {
     isVisible,
     toggleVisibility
   };
 };
 
+
 const Login = () => {
 
   const { isVisible, toggleVisibility } = useVisible();
+
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/register");
+  };
+
 
   return (
     <div className='absolute h-[100vh] w-[100vw] bg-gradient-to-r from-sulu-300 via-mindaro-300 to-concrete-100'>
       <div className='flex justify-center items-center h-full w-full'>
         <Card radius='sm' className="w-96 bg-white/30 backdrop-blur-sm">
-          <CardHeader className="flex gap-3">
-            <h3 className='font-Kanit-Bold text-2xl'>Login</h3>
+          <CardHeader className="flex justify-center">
+            <h3 className='font-Kanit-Bold text-3xl text-mindaro-800'>Login</h3>
           </CardHeader>
           <Divider />
-          <CardBody>
+          <CardBody className='p-5'>
             <form action="" className='flex flex-col gap-5'>
               <Input
                 isRequired
@@ -41,42 +48,56 @@ const Login = () => {
                 label="Email"
                 variant='underlined'
                 placeholder="Enter your email"
+                errorMessage='Fill out this field and include an "@" sign in the email address.'
                 classNames={{
-                  base:'font-Kanit-Light',
-                  label:'font-Kanit-Medium',
-                  description:'',
-                  input:'',
-                  mainWrapper:'',
-                  innerWrapper:'',
-                  inputWrapper:'border-sulu-500 hover:border-sulu-300',
-                  errorMessage:''
+                  base: 'font-Kanit-Light',
+                  label: 'font-Kanit-Medium',
+                  inputWrapper: 'border-sulu-500 hover:border-sulu-300',
+                  errorMessage: 'font-Kanit-Light'
                 }}
-                // className=''
               />
 
               <Input
                 isRequired
                 size='sm'
                 label="Password"
+                autoComplete='off'
                 variant='underlined'
                 placeholder="Enter your password"
+                errorMessage='Fill out this field.'
                 endContent={
                   <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
                     {isVisible ? (
-                      <i className='bi bi-eye-slash text-2xl text-default-400 pointer-events-none'></i>
+                      <i className='bi bi-eye-slash text-2xl text-default-600 pointer-events-none'></i>
                     ) : (
-                      <i className='bi bi-eye text-2xl text-default-400 pointer-events-none'></i>
+                      <i className='bi bi-eye text-2xl text-default-600 pointer-events-none'></i>
                     )}
                   </button>
                 }
                 type={isVisible ? "text" : "password"}
-                className=""
+                classNames={{
+                  base: 'font-Kanit-Light bg-transparent',
+                  label: 'font-Kanit-Medium',
+                  inputWrapper: 'border-sulu-500 hover:border-sulu-300',
+                  errorMessage: 'font-Kanit-Light'
+                }}
               />
-               <Button 
+              <Button
                 size="sm"
-                variant='ghost'>
-                Small
+                variant='solid'
+                className='bg-mindaro-600 text-white font-Kanit-Medium text-lg shadow-lg'>
+                Sign In
               </Button>
+              <div className='flex flex-col'>
+                <small className='font-Kanit-Light'>Don't you have an account?</small>
+                <Button
+                  size="sm"
+                  variant='bordered'
+                  onClick={handleNavigate}
+                  className='text-mindaro-500 font-Kanit-Medium text-lg shadow-lg border-mindaro-500 hover:text-white hover:bg-mindaro-500'>
+                  Sign Up
+                </Button>
+              </div>
             </form>
           </CardBody>
         </Card>
