b3820a325ef43f81dd1263337fe0f838ec279c80
Cristofer Alan Angeles Soreano <cangeles@connectiveit.com.mx>
2024-05-24 14:30:51 -0600

    home pending

--- public/index.tsx
+++ public/index.tsx
@@ -1,1 +1,4 @@
 export { default as logo } from './img/logo.png';
+export { default as blog1 } from './img/blog-1.jpg';
+export { default as blog2 } from './img/blog-2.jpg';
+export { default as blog3 } from './img/blog-3.jpg';
--- src/pages/HomePages/Home.tsx
+++ src/pages/HomePages/Home.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 
+import { Image } from "@nextui-org/react";
+
+import { blog1 } from '../../../public'
+
 const Home = () => {
   return (
     <section className='bg-concrete-50'>
-      <div>
-        sadas
+      <div className='flex flex-col items-center'>
+        <div className='w-[85%] bg-white'>
+          <div className='w-full h-[10rem] lg:w-1/2 overflow-hidden'>
+            <Image
+              radius='none'
+              src={blog1}
+              fallbackSrc="https://via.placeholder.com/"
+              alt="WomenWriter"
+              classNames={{
+                img: ''
+              }}
+              className=' lg:w-1/3'
+            />
+          </div>
+          <div className='w-full'>
+            <h3 className='stroke-text font-Kanit-Bold leading-none uppercase text-2xl md:text-4xl lg:text-6xl'><span>Explora el Mundo de las Ideas:</span><br/><span>Tu Portal de Blogs</span><br/><span>Inspiradores</span></h3>
+            {/* <p className='font-Kanit-Regular leading-tight text-chicago-800'>Bienvenido a nuestro rincón de creatividad y conocimiento, donde cada entrada de blog es una puerta a nuevas perspectivas. Desde ciencia y tecnología hasta arte y gastronomía, sumérgete en contenidos cuidadosamente elaborados para inspirar, educar y entretener. Descubre, aprende y comparte en la comunidad que celebra la pasión por escribir y leer sobre los temas que más te interesan.</p> 
+            https://magicui.design/docs/components/flip-text*/}
+          </div>
+        </div>
       </div>
     </section>
   )
